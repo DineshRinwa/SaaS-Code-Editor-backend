030c66018da90f9b68c2af2f10c8be90f67dc27c
Dinesh Rinwa <112754603+DineshRinwa@users.noreply.github.com>
2025-05-04 22:16:10 +0530

    Update auth.js

--- saas-ide-backend/middleware/auth.js
+++ saas-ide-backend/middleware/auth.js
@@ -8,6 +8,7 @@ const authMiddleware = async (req, res, next) => {
   if (accessToken) {
     try {
       const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      console.log(process.env.ACCESS_TOKEN_SECRET);
       req.user = decoded;
       return next();
     } catch (error) {
@@ -27,6 +28,7 @@ const authMiddleware = async (req, res, next) => {
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET
       );
+      console.log(process.env.REFRESH_TOKEN_SECRET);
 
       // Generate new access token
       const newAccessToken = jwt.sign(
